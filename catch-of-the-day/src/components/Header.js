fe101240ef1d48fba9a254d99abb3ee13630c120
Mark Shropshire <mdshrops@shropnet.net>
2017-07-23 13:57:19 -0400

    Remove link form header

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 const Header = (props) => {
   return (
     <header className="top">
       <h1>
-        <Link to="/">
         Catch
         <span className="ofThe">
           <span className="of">of</span>
           <span className="the">the</span>
         </span>
         Day
-      </Link>
       </h1>
       <h3 className="tagline"><span>{props.tagline}</span></h3>
     </header>
