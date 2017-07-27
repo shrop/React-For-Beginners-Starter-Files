c0b43615743f42f84e8de8bb0ead1677efe37092
Mark Shropshire <mdshrops@shropnet.net>
2017-07-26 20:19:27 -0400

    Completed lesson on state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,25 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // Update our state.
+    const fishes = {...this.state.fishes};
+    // Add in our new fish.
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // Set state.
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +31,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
