7dcbe75bce08f016dedfe8fe33a2a4e6a0fc0c63
Mark Shropshire <mdshrops@shropnet.net>
2017-08-08 08:19:52 -0400

    Added the ability to use the Add To Order button and add items to the order state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     super();
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     this.state = {
       fishes: {},
       order: {}
@@ -33,6 +34,15 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    // Take a copy of our state.
+    const order = {...this.state.order};
+    // Update or add the new number of fish ordered.
+    order[key] = order[key] +1 || 1;
+    // Update our state.
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -42,7 +52,7 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
